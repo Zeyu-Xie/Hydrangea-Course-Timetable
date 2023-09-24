35a3a56bfa5482516ab823b795adc696b09262b2
Acan <xie.zeyu20@gmail.com>
2023-09-24 22:27:36 +0800

    Basically Finished

--- Hydrangea Course Timetable/ContentView.swift
+++ Hydrangea Course Timetable/ContentView.swift
@@ -8,14 +8,49 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var currentDateTime = Date()
+    
     var body: some View {
         VStack {
-            Image("test").resizable() // 允许图片自动调整大小
-                        .aspectRatio(contentMode: .fit) // 设置图片显示模式
-                        .frame(width: 200, height: 200) // 设置图片框架大小
-            Text("Hello, world!")
+            
+            VStack(alignment: .leading) {
+                Text("Hydrangea Course Timetable")
+                    .font(.title)
+                
+                Text("")
+                
+                Text("- \(formattedDateTime(currentDateTime))")
+                    .onAppear {
+                        startUpdatingDateTime()
+                    }
+                Text("")
+            }
+            .frame(width: 800, alignment: .leading)
+            
+            VStack(alignment: .center) {
+                Image("timetable")
+                    .resizable()
+                    .aspectRatio( contentMode: .fit)
+                    .frame(width: 800.0)
+            }
+            
+            
         }
         .padding()
+    }
+    
+    func formattedDateTime(_ date: Date) -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = .medium
+        dateFormatter.timeStyle = .medium
+        return dateFormatter.string(from: date)
+    }
+    
+    func startUpdatingDateTime() {
+        Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { timer in
+            currentDateTime = Date()
+        }
     }
 }
 

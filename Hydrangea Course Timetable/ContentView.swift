c28d27509e66e543ab16d8c7f01bbd906826b7df
Acan <xie.zeyu20@gmail.com>
2023-09-25 13:47:09 +0800

    .

--- Hydrangea Course Timetable/ContentView.swift
+++ Hydrangea Course Timetable/ContentView.swift
@@ -2,14 +2,16 @@
 //  ContentView.swift
 //  Hydrangea Course Timetable
 //
-//  Created by 谢泽钰 on 2023/9/24.
+//  Created by Acan on 2023/9/24.
 //
 
 import SwiftUI
+import UniformTypeIdentifiers
 
 struct ContentView: View {
     
     @State private var currentDateTime = Date()
+    @State private var selectedImage: NSImage?
     
     var body: some View {
         VStack {
@@ -25,6 +27,30 @@ struct ContentView: View {
                         startUpdatingDateTime()
                     }
                 Text("")
+                
+                Button("Select Image") {
+                    let openPanel = NSOpenPanel()
+                    openPanel.allowedContentTypes = [UTType.image]
+                    openPanel.canChooseDirectories = false
+                    
+                    if openPanel.runModal() == .OK {
+                        if let imageURL = openPanel.url {
+                            selectedImage = NSImage(contentsOf: imageURL)
+                            
+                            if let documentsDirectoryURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first {
+                                let fileURL = documentsDirectoryURL.appendingPathComponent("uploaded_timetable.png")
+                                if let image = selectedImage, let imageData = image.tiffRepresentation {
+                                    do {
+                                        try imageData.write(to: fileURL)
+                                        print("Image Saved Successfully：\(fileURL.path)")
+                                    } catch {
+                                        print("Image Saved Failed：\(error)")
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
             }
             .frame(width: 800, alignment: .leading)
             
@@ -34,7 +60,6 @@ struct ContentView: View {
                     .aspectRatio( contentMode: .fit)
                     .frame(width: 800.0)
             }
-            
             
         }
         .padding()

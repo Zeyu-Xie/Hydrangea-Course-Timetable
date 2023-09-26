9d687000f103325bfac92513f1331f2309f94b29
Acan <xie.zeyu20@gmail.com>
2023-09-26 19:44:44 +0800

    Version 2.0.0 finished

--- Hydrangea Course Timetable/ContentView.swift
+++ Hydrangea Course Timetable/ContentView.swift
@@ -6,14 +6,29 @@
 //
 
 import SwiftUI
+import Foundation
 import UniformTypeIdentifiers
+
+struct course: Codable {
+    var name: String
+    var learn: String
+    var repo: String
+    var page: String
+}
+
+struct courses: Codable {
+    var data: Array<course>
+}
 
 struct ContentView: View {
     
+    @State private var localCourses : courses = courses(data: [])
     @State private var currentDateTime = Date()
     @State private var selectedImage: NSImage?
+    @State private var selectedConfig: NSData?
     @State private var _documentsURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first
     @State private var _imageURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!.appendingPathComponent("image.png")
+    @State private var _configURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!.appendingPathComponent("config.json")
     
     var body: some View {
         VStack {
@@ -33,34 +48,88 @@ struct ContentView: View {
                         startUpdatingDateTime()
                     }
                 Text("")
-                
-                Button("Select Image") {
-                    let openPanel = NSOpenPanel()
-                    openPanel.allowedContentTypes = [UTType.image]
-                    openPanel.canChooseDirectories = false
-                    
-                    if openPanel.runModal() == .OK {
-                        if let imageURL = openPanel.url {
-                            selectedImage = NSImage(contentsOf: imageURL)
-                            
-                            if let documentsDirectoryURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first {
-                                let fileURL = documentsDirectoryURL.appendingPathComponent("image.png")
-                                if let image = selectedImage, let imageData = image.tiffRepresentation {
-                                    do {
-                                        try imageData.write(to: fileURL)
-                                        print("Image Saved Successfully：\(fileURL.path)")
-                                    } catch {
-                                        print("Image Saved Failed：\(error)")
+                HStack {
+                    Button("Select Image") {
+                        let openPanel = NSOpenPanel()
+                        openPanel.allowedContentTypes = [UTType.image]
+                        openPanel.canChooseDirectories = false
+                        
+                        if openPanel.runModal() == .OK {
+                            if let imageURL = openPanel.url {
+                                selectedImage = NSImage(contentsOf: imageURL)
+                                
+                                if let documentsDirectoryURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first {
+                                    let fileURL = documentsDirectoryURL.appendingPathComponent("image.png")
+                                    if let image = selectedImage, let imageData = image.tiffRepresentation {
+                                        do {
+                                            try imageData.write(to: fileURL)
+                                            print("Image Saved Successfully：\(fileURL.path)")
+                                        } catch {
+                                            print("Image Saved Failed：\(error)")
+                                        }
                                     }
                                 }
                             }
                         }
                     }
+                    
+                    Button("Upload Config") {
+                        
+                        
+                        let openPanel = NSOpenPanel()
+                        openPanel.allowedContentTypes = [UTType.json]
+                        openPanel.canChooseDirectories = false
+                        
+                        if openPanel.runModal() == .OK {
+                            
+                            if let configURL = openPanel.url {
+                                selectedConfig=NSData(contentsOf: configURL)
+                            }
+                            
+                            
+                            
+                            
+                            
+                            if let documentsDirectoryURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first {
+                                let fileURL = documentsDirectoryURL.appendingPathComponent("config.json")
+                                if let config = selectedConfig {
+                                    do {
+                                        try config.write(to: fileURL)
+                                        print("Config Uploaded Successfully：\(fileURL.path)")
+                                    } catch {
+                                        print("Config Uploaded Failed：\(error)")
+                                    }
+                                }
+                            }
+                            
+                        }
+                        
+                        
+                        
+                        
+                        
+                        
+                        
+                    }
+                    
+                    Button("Folder Path") {
+                        let pasteboard = NSPasteboard.general
+                        pasteboard.clearContents()
+                        pasteboard.setString(_documentsURL!.path, forType: .string)
+                        let alert = NSAlert()
+                        alert.messageText = "Folder Path Copied"
+                        alert.informativeText = "Successfully copied folder path to clickboard."
+                        alert.alertStyle = .informational
+                        alert.addButton(withTitle: "Close")
+                        alert.runModal()
+                    }
                 }
                 Text("")
                 Divider()
             }
-            .frame(width: 1280, alignment: .leading)
+            .frame(width: 1280, alignment: .leading).onAppear {
+                print("Location: \(_documentsURL?.path)")
+            }
             
             VStack(alignment: .center) {
                 Text("")
@@ -72,10 +141,31 @@ struct ContentView: View {
                 } else {
                     Text("Please upload your course timetable by clicking the \"Select Image\" button.")
                 }
+                Text("")
+                Divider()
+            }.frame(width: 1280, alignment: .leading).onAppear {
+                print("Location: \(_documentsURL?.path)")
             }
             
+            HStack {
+                ForEach(0..<localCourses.data.count, id:\.self) {num in
+                    VStack(alignment: .leading) {
+                        Link(localCourses.data[num].name, destination: URL(string: String(localCourses.data[num].learn))!)
+                        HStack {
+                            Link("GitHub", destination: URL(string: String(localCourses.data[num].repo))!)
+                            Link("Notes", destination: URL(string: String(localCourses.data[num].page))!)
+                        }
+                    }
+                    Spacer()
+                }
+            }.frame(width: 1280, alignment: .leading).onAppear {
+                print("Location: \(_documentsURL?.path)")
+            }.padding()
+            
         }
-        .padding()
+        .padding().onAppear {
+            getConfig()
+        }
     }
     
     func formattedDateTime(_ date: Date) -> String {
@@ -90,6 +180,18 @@ struct ContentView: View {
             currentDateTime = Date()
         }
     }
+    
+    func getConfig() {
+        do {
+            let configData=try Data(contentsOf: _configURL)
+            let decoder=JSONDecoder()
+            localCourses=try decoder.decode(courses.self, from: configData)
+        }
+        catch {
+            print("ERROR")
+        }
+    }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {

f912e1a2777faa2b534933218e742b7ef3cec223
acanxie <xie.zeyu20@gmail.com>
2023-11-01 15:53:41 +0800

    .

--- main.py
+++ main.py
@@ -1,52 +1,65 @@
 import json
 import webbrowser
 
+def getCourses():
+    cs = []
+    with open("./courses.json", "r", encoding="utf-8") as file:
+        cs = json.load(file)
+    return cs
+
 def inputIndex():
-    idx = 0
+
+    print("\n")
+    print("> Welcome to Hydrangea Course Timetable")
+    print("> Made by Acan")
+    print("\n")
+    for i in range(0, len(courses)):
+        print(f"{i+1}. {courses[i]['name']}")
+    print("\n")
+
+    idx = -1
     while (idx < 1 or idx > num):
         idx = input("> Please input your course index: ")
         idx = int(idx)
     idx = idx - 1
-    print("  ok")
+    print("> ok")
     print("\n")
     return idx
 
-def inputType():
-    tp = 0
-    while (tp < 1 or tp > 3):
+def inputType(idx):
+    tp = -1
+    while (tp < 0 or tp > 3):
+        print(f"> Your choice is {courses[idx]['name']}")
         print("> Which page do you need to open?")
         print("  1. 网络学堂")
         print("  2. GitHub")
         print("  3. GitHub Pages")
+        print("  0. (Back)")
         tp = input("> Input the index: ")
         tp = int(tp)
+    print("> ok")
+    print("\n")
     return tp
 
-courses = []
+def openWebsite(idx, tp):
+    if (tp == 1):
+        webbrowser.open(courses[idx]["learnx"])
+    elif (tp == 2):
+        webbrowser.open(courses[idx]["github"])
+    else:
+        webbrowser.open(courses[idx]["notes"])
 
-with open("./courses.json", "r", encoding="utf-8") as file:
-    courses = json.load(file)
 
+courses = getCourses()
 num = len(courses)
 
-print("\n")
-print("> Welcome to Hydrangea Course Timetable")
-print("> Made by Acan")
-print("\n")
-for i in range(0, len(courses)):
-    print(f"{i+1}. {courses[i]['name']}")
-print("\n")
+certain = 0
 
-index = inputIndex()
-
-type = inputType()
-
-
-if (type == 1):
-    webbrowser.open(courses[index]["learnx"])
-elif (type == 2):
-    webbrowser.open(courses[index]["github"])
-else:
-    webbrowser.open(courses[index]["notes"])
-
-
+while(certain == 0):
+    index = inputIndex()
+    type = inputType(index)
+    if(type == 0): 
+        certain = 0
+    else:
+        certain = 1
+        openWebsite(index, type)
